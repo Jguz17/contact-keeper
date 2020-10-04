527e768f3debb28256b50089e04cb1b16a7d077d
jguz17 <johnathanguzman17@gmail.com>
2020-10-04 13:43:03 -0500

    added auth middleware

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken')
 const config = require('config')
 const  { check, validationResult } = require('express-validator')
 
+const auth = require('../middleware/auth')
 const User = require('../models/User')
 
 const router = express.Router()
@@ -11,8 +12,14 @@ const router = express.Router()
 // @route           GET api/auth
 // @description     Get logged in user
 // @access          Private
-router.get('/', (req, res) => {
-    res.send('Get logged in user')
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Server error")
+    }
 })
 
 // @route           POST api/auth
