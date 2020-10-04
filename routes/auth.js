790aee1e8a4f134b21851909c7b4325349a0ebb4
jguz17 <johnathanguzman17@gmail.com>
2020-10-03 23:58:09 -0500

    added auth. need middleware

--- routes/auth.js
+++ routes/auth.js
@@ -18,8 +18,52 @@ router.get('/', (req, res) => {
 // @route           POST api/auth
 // @description     Auth user & get token
 // @access          Public
-router.post('/', (req, res) => {
-    res.send('Log in user')
+router.post('/', [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+], async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+
+    const { email, password } = req.body
+
+    try {
+        let user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(400).json({
+                message: 'Invalid credentials'
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: 'Invalid credentials'
+            })
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (error, token) => {
+            if (error) throw error
+            res.json({ token })
+        })
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server error')
+    }
 })
 
 module.exports = router
