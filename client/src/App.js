6e80f3f917b0ab6278fc98fa0c315ec1e5687a8e
jguz17 <johnathanguzman17@gmail.com>
2020-10-05 17:45:51 -0500

    added navbar and client side routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import Home from './components/pages/Home.js'
+import About from './components/pages/About'
 import './App.css';
 
 import Navbar from './components/layout/Navbar'
@@ -11,7 +13,10 @@ const App = () => {
         <Fragment>
           <Navbar/>
           <div className='container'>
-
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/about' component={About}/>
+            </Switch>
           </div>
       </Fragment>
     </Router>
