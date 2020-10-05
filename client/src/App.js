6ffe117dd73b57b494e56fd33284dcb01b8f67df
jguz17 <johnathanguzman17@gmail.com>
2020-10-05 17:30:57 -0500

    added frontend files

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import './App.css';
+
+import Navbar from './components/layout/Navbar'
+
+const App = () => {
+  return (
+    <Router>
+        <Fragment>
+          <Navbar/>
+          <div className='container'>
+
+          </div>
+      </Fragment>
+    </Router>
+  );
+}
+
+export default App;
