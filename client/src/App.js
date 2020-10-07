d401ea01f403bb9be870e8666e9d2a946a90f54f
jguz17 <johnathanguzman17@gmail.com>
2020-10-07 00:09:51 -0500

    added private routes

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import AuthState from './context/auth/AuthState'
 import AlertState from './context/alert/AlertState'
 import Alerts from './components/layout/Alerts'
 import setAuthToken from './utils/setAuthToken'
+import PrivateRoute from './components/routing/PrivateRoute'
 import './App.css';
 
 import Navbar from './components/layout/Navbar'
@@ -29,7 +30,7 @@ const App = () => {
                 <Alerts/>
                 <div className='container'>
                   <Switch>
-                    <Route exact path='/' component={Home}/>
+                    <PrivateRoute exact path='/' component={Home}/>
                     <Route exact path='/about' component={About}/>
                     <Route exact path='/register' component={Register}/>
                     <Route exact path='/login' component={Login}/>
